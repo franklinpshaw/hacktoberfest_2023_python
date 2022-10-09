6de14a212244c9de9af57c484a864d7392929f75
Ujwal Gulhane <72155234+Ujwalgulhane@users.noreply.github.com>
2022-10-09 11:15:50 +0530

    updated in more approachable manner
    Time complexity of program is -> O(log n)

--- Binary-Serach.cpp
+++ Binary-Serach.cpp
@@ -1,33 +1,47 @@
-#include <iostream>  
-#include <vector>  
-#include <algorithm>  
-   
-using namespace std;  
-   
-int main()  
-{  
-  int a[] = {1, 2, 3, 4, 5, 6, 7, 9, 10};  
-  vector<int> v(a, a+9);  
-  cout <<"\nHere are the values in the vector:\n";  
-  for (vector<int>::size_type i=0; i<v.size(); i++)  
-    cout <<v.at(i)<<" ";  
-   
-  if (binary_search(v.begin(), v.end(), 3))  
-    cout <<"\nThe value 3 was found.";  
-  else  
-    cout <<"\nThe value 3 was not found.";  
-   
-  if (binary_search(v.begin(), v.end(), 8))  
-    cout <<"\nThe value 8 was found.";  
-  else  
-    cout <<"\nThe value 8 was not found.";  
-   
-  return 0;  
-}  
 
+#include <iostream>
+using namespace std;
+bool isBinarysearch(int arr[], int size, int key){
+    int s = 0, e = size - 1, mid = s + (e-s)/2;
+    while(s <= e){
+        if(arr[mid]==key){
+            return 1;
+        }
+        else if(arr[mid] < key){
+            s = mid + 1;
+        }
+        else{
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return 0;
+}
 
-Output:
-Here are the values in the vector:
-1 2 3 4 5 6 7 9 10 
-The value 3 was found.
-The value 8 was not found.
+int main()
+{
+    int n;
+    cout << "Enter the size of an array -> ";
+    cin >> n;
+    int arr[n];
+    cout <<"Enter elements in an array -> ";
+    for(int i = 0; i < n; i++){
+        cin >> arr[i]; 
+    }
+    cout << "Enter number would you like to search -> ";
+    int key;
+    cin >> key;
+    if(isBinarysearch(arr, n, key)){
+        cout << "The values " << key << " was found in given an array" << endl;
+    }
+    else{
+        cout << "The values " << key << " was not found in given an array" << endl;
+    }
+    
+
+    return 0;
+}
+output -> 
+Enter elements in an array -> 1 2 3 4 5 6 7 9 10 11
+Enter number would you like to search -> 12
+The values 12 was not found
