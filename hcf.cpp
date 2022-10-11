fd28e399cc5b8bf07fed88d0488d8faa2ee30922
Ankit Kumar <114617072+Ankitkr234@users.noreply.github.com>
2022-10-11 22:59:02 +0530

    Create hcf.cpp

---
+++ hcf.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+//recursive call to find HCF
+int gcd(int a, int b){
+   if (a == 0 || b == 0)
+      return 0;
+   if (a == b)
+      return a;
+   if (a > b)
+      return gcd(a-b, b);
+   return gcd(a, b-a);
+}
+int main(){
+   int a = 98, b = 56;
+   printf("GCD of %d and %d is %d ", a, b, gcd(a, b));
+   return 0;
+}
